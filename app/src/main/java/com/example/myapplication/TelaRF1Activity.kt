741a3f0c50fb5e0ee7ca3124509a9b73b4cf9473
Mayara Medino <medino.dev@gmail.com>
2025-04-10 18:40:10 -0300

    atualizações

--- app/src/main/java/com/example/myapplication/TelaRF1Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF1Activity.kt
@@ -1,4 +1,13 @@
 package com.example.myapplication
 
-class TelaRF1Activity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TelaRF1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_tela_rf1)
+    }
+
 }
