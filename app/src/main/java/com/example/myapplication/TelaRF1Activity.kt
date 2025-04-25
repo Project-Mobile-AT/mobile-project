b63ebf6427e8cdb64725010c70fca47c20199e46
Mayara Medino <medino.dev@gmail.com>
2025-04-25 00:45:26 -0300

    Funcionalidade de cliques na tela RF1 inserida

--- app/src/main/java/com/example/myapplication/TelaRF1Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF1Activity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class TelaRF1Activity : AppCompatActivity() {
@@ -8,6 +11,25 @@ class TelaRF1Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_tela_rf1)
+
+        val btn_login = findViewById<Button>(R.id.btn_login)
+        val recuperarSenha = findViewById<TextView>(R.id.tv_esqueceu_senha)
+        val criarConta = findViewById<TextView>(R.id.tv_registre_se)
+
+        btn_login.setOnClickListener {
+            val intent = Intent(this, TelaRF3Activity::class.java)
+            startActivity(intent)
+        }
+
+        recuperarSenha.setOnClickListener {
+            val intent = Intent(this, TelaRF1_1Activity::class.java)
+            startActivity(intent)
+        }
+
+        criarConta.setOnClickListener {
+            val intent = Intent(this, TelaRF2Activity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
