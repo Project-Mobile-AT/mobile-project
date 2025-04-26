03e34ae2d7c03b962bf432fe5e772f77035a814d
Mayara Medino <medino.dev@gmail.com>
2025-04-25 21:03:52 -0300

    movimentações na tela RF 3 inserida

--- app/src/main/java/com/example/myapplication/TelaRF3Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF3Activity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,13 @@ class TelaRF3Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_rf3)
+
+        val perfil = findViewById<ImageView>(R.id.ic_per)
+
+        perfil.setOnClickListener {
+            val intent = Intent(this, TelaRF3_1Activity::class.java)
+            startActivity(intent)
+        }
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.container_bottom_nav, BottomNavFragment())
--- app/src/main/java/com/example/myapplication/TelaRF3_1Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF3_1Activity.kt
@@ -1,20 +1,18 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.fragments.BottomNavFragment
 
 class TelaRF3_1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf3_1)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+
     }
 }
