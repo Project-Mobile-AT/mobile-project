da1b1528ebf715e86d4925b0d77d90eba70c779e
Mayara Medino <medino.dev@gmail.com>
2025-04-27 11:58:36 -0300

    Menu nav_bottom, navegação e menu hamburguer inserido na tela 3.1

--- app/src/main/java/com/example/myapplication/TelaRF3_1Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF3_1Activity.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.fragments.BottomNavFragment
@@ -12,7 +14,34 @@ class TelaRF3_1Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_rf3_1)
 
+        val menuButton = findViewById<ImageView>(R.id.id_menu)
 
+        menuButton.setOnClickListener { view ->
+            val popup = PopupMenu(this, view)
+            popup.menuInflater.inflate(R.menu.menu_popup, popup.menu)
+
+            popup.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.menu_informativos -> {
+                        Toast.makeText(this, "Abrindo Informativos", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.menu_sair -> {
+                        Toast.makeText(this, "Saindo...", Toast.LENGTH_SHORT).show()
+                        finish()
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            popup.show()
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container_bottom_nav, BottomNavFragment())
+            .commit()
 
     }
+
 }
--- app/src/main/java/com/example/myapplication/TelaRF5Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF5Activity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,15 +14,17 @@ class TelaRF5Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf5)
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-           // val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-           // v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            //insets
-        //}
+
+        val perfil = findViewById<ImageView>(R.id.ic_per)
         val btn_consulta = findViewById<Button>(R.id.btn_consulta)
 
         btn_consulta.setOnClickListener {
             val intent = Intent(this, TelaRF5_1Activity::class.java)
+            startActivity(intent)
+        }
+
+        perfil.setOnClickListener {
+            val intent = Intent(this, TelaRF3_1Activity::class.java)
             startActivity(intent)
         }
     }
