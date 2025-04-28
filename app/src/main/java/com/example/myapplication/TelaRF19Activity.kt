9ac85379a7b2df9d2bc14195c488926d238189ef
Mayara Medino <medino.dev@gmail.com>
2025-04-28 17:09:17 -0300

    Navegação, acessibilidade e chat inseridos na tela 19.1

--- app/src/main/java/com/example/myapplication/TelaRF19Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF19Activity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -25,6 +26,8 @@ class TelaRF19Activity : AppCompatActivity() {
 
         val fabChat = findViewById<FloatingActionButton>(R.id.fab_chat)
 
+        val inicio = findViewById<ImageView>(R.id.homeIcon)
+
         addInform.setOnClickListener {
             val intent = Intent(this, TelaRF19_2Activity::class.java)
             startActivity(intent)
@@ -32,6 +35,11 @@ class TelaRF19Activity : AppCompatActivity() {
 
         editInform.setOnClickListener {
             val intent = Intent(this, TelaRF19_2Activity::class.java)
+            startActivity(intent)
+        }
+
+        inicio.setOnClickListener {
+            val intent = Intent(this, TelaRF11Activity::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/myapplication/TelaRF19_2Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF19_2Activity.kt
@@ -1,20 +1,42 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.fragments.ChatbotPopupFragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TelaRF19_2Activity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf19_2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val inicio = findViewById<ImageView>(R.id.homeIcon)
+
+        val fabChat = findViewById<FloatingActionButton>(R.id.fab_chat)
+
+        val voltar = findViewById<ImageView>(R.id.btVoltar)
+
+        inicio.setOnClickListener {
+            val intent = Intent(this, TelaRF11Activity::class.java)
+            startActivity(intent)
+        }
+
+        fabChat.setOnClickListener {
+            val chatbotPopup = ChatbotPopupFragment()
+            chatbotPopup.show(supportFragmentManager, "ChatbotPopup")
+        }
+
+        voltar.setOnClickListener {
+            val intent = Intent(this, TelaRF19Activity::class.java)
+            startActivity(intent)
         }
     }
 }
