dbb1f3527e7af7f36f137a6424ff316518149230
Mayara Medino <medino.dev@gmail.com>
2025-04-28 18:45:03 -0300

    Ajuste, navegação e icones de acessibilidade e chat inserido na tela 6

--- app/src/main/java/com/example/myapplication/TelaRF6Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF6Activity.kt
@@ -1,20 +1,69 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.fragments.BottomNavFragment
 
 class TelaRF6Activity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_tela_rf6)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val menuButton = findViewById<ImageView>(R.id.ivMenu)
+
+        val perfil = findViewById<ImageView>(R.id.ivProfile)
+
+        perfil.setOnClickListener {
+            val intent = Intent(this, TelaRF8Activity::class.java)
+            startActivity(intent)
         }
+
+        //Inserir menu hamburguer
+        menuButton.setOnClickListener { view ->
+            val popup = PopupMenu(this, view)
+            popup.menuInflater.inflate(R.menu.menu_popup, popup.menu)
+
+            popup.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.menu_dados -> {
+                        Toast.makeText(this, "Carregando Dados Pessoais", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, TelaRF3_1Activity::class.java)
+                        startActivity(intent)
+                        true
+                    }
+                    R.id.menu_informativos -> {
+                        Toast.makeText(this, "Abrindo Informativos", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, TelaRF8_4Activity::class.java)
+                        startActivity(intent)
+                        true
+                    }
+                    R.id.menu_sair -> {
+                        Toast.makeText(this, "Saindo...", Toast.LENGTH_SHORT).show()
+                        finish()
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            popup.show()
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container_bottom_nav, BottomNavFragment())
+            .commit()
     }
+
+
 }
