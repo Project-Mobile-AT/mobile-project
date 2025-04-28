cf2eee0a55e1e7793b04f12e2e2acb5ce8bb7fd5
Liviacavalcanter <liviacavalcantebr@gmail.com>
2025-04-27 21:01:40 -0300

    navegações nas telas RF7.1 e RF8; edição do menu na tela RF3

--- app/src/main/java/com/example/myapplication/TelaRF7_1Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF7_1Activity.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TelaRF7_1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,11 @@ class TelaRF7_1Activity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf7_1)
 
+        val helpButton = findViewById<ImageButton>(R.id.helpButton)
+
+        helpButton.setOnClickListener {
+            val intent = Intent(this, TelaRF10Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/TelaRF8Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF8Activity.kt
@@ -1,20 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class TelaRF8Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf8)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btn_personal = findViewById<Button>(R.id.btn_personal)
+        btn_personal.setOnClickListener {
+            val intent = Intent(this, TelaRF5_1Activity::class.java)
+            startActivity(intent)
         }
     }
 }
