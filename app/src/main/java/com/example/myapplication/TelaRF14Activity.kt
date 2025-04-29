bdd0393fb0b60571269d950fa6ca695578fc13cd
Liviacavalcanter <liviacavalcantebr@gmail.com>
2025-04-29 02:36:35 -0300

    navegação da RF14 para RF14_1 e RF14_2

--- app/src/main/java/com/example/myapplication/TelaRF14Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF14Activity.kt
@@ -1,16 +1,31 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class TelaRF14Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf14)
+        val addAula = findViewById<MaterialButton>(R.id.btn_add_class)
+        val editarAula = findViewById<ImageView>(R.id.edit_class)
+
+        addAula.setOnClickListener {
+            val intent = Intent(this, TelaRF14_2Activity::class.java)
+            startActivity(intent)
+        }
+        editarAula.setOnClickListener {
+            val intent = Intent(this, TelaRF14_1Activity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/example/myapplication/TelaRF14_2Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF14_2Activity.kt
@@ -11,10 +11,6 @@ class TelaRF14_2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf14_2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
