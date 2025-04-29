8678a83f71a23ee772ee834b7e1659a97421e635
Liviacavalcanter <liviacavalcantebr@gmail.com>
2025-04-29 02:45:00 -0300

    navegação da RF13 para RF13_1 e RF13_2

--- app/src/main/java/com/example/myapplication/TelaRF13Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF13Activity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,19 @@ class TelaRF13Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf13)
+
+        val addEquipamento = findViewById<Button>(R.id.btnAdicionarEquipamento)
+        val editarEquipamento = findViewById<ImageButton>(R.id.edit_equipamento)
+
+        addEquipamento.setOnClickListener {
+            val intent = Intent(this, TelaRF13_2Activity::class.java)
+            startActivity(intent)
+        }
+
+        editarEquipamento.setOnClickListener {
+            val intent = Intent(this, TelaRF13_1Activity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/example/myapplication/TelaRF13_1Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF13_1Activity.kt
@@ -11,10 +11,6 @@ class TelaRF13_1Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf13_1)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
