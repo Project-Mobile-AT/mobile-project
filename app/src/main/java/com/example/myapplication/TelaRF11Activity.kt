3b68c3397366391e3c96a15fd41bff1f7d205829
Mayara Medino <medino.dev@gmail.com>
2025-04-28 21:41:35 -0300

    Projeto rodando em adm

--- app/src/main/java/com/example/myapplication/TelaRF11Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF11Activity.kt
@@ -11,10 +11,6 @@ class TelaRF11Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf11)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
