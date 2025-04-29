25245d00b4056f6aa8bcfa337a9339b308e786e0
Liviacavalcanter <liviacavalcantebr@gmail.com>
2025-04-29 17:44:13 -0300

    navegações das páginas RF16 a RF16_6

--- app/src/main/java/com/example/myapplication/TelaRF16_2Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF16_2Activity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,19 @@ class TelaRF16_2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf16_2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val editar = findViewById<ImageView>(R.id.editar)
+
+        editar.setOnClickListener {
+            val intent = Intent(this, TelaRF16_4Activity::class.java)
+            startActivity(intent)
+        }
+
+        val addHorario = findViewById<Button>(R.id.button_add_appointment)
+
+        addHorario.setOnClickListener {
+            val intent = Intent(this, TelaRF16_3Activity::class.java)
+            startActivity(intent)
         }
     }
 }
