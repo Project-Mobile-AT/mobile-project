447b58024bd3772e79ef62a766d502b4e6984e07
Marfim <marfim.dev@gmail.com>
2025-04-30 11:20:12 -0300

    Ajustar Activity - RF17.3 | RF17.4 | RF17.7

--- app/src/main/java/com/example/myapplication/TelaRF17_3Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF17_3Activity.kt
@@ -1,20 +1,27 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class TelaRF17_3Activity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf17_3)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        enableEdgeToEdge()
+
+        val btn_back = findViewById<ImageButton>(R.id.btn_back)
+
+        btn_back.setOnClickListener { val intent = Intent(this, TelaRF17_2Activity::class.java)
+            startActivity(intent)}
     }
 }
+
--- app/src/main/java/com/example/myapplication/TelaRF17_7Activity.kt
+++ app/src/main/java/com/example/myapplication/TelaRF17_7Activity.kt
@@ -1,20 +1,27 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class TelaRF17_7Activity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_tela_rf17_7)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        enableEdgeToEdge()
+
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+
+        backButton.setOnClickListener { val intent = Intent(this, TelaRF17_5Activity::class.java)
+            startActivity(intent)}
     }
 }
+
