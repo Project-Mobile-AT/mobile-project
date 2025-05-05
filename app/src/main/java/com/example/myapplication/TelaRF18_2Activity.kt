9fd179cf7f29bab4443b5fea0a51636f9764e6c1
Liviacavalcanter <liviacavalcantebr@gmail.com>
2025-05-05 16:16:37 -0300

    criação da tela 18_2 (perfil aluno)

---
+++ app/src/main/java/com/example/myapplication/TelaRF18_2Activity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.os.Bundle
+
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class TelaRF18_2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela_rf18_2)
+
+    }
+}
